9856a2e2e1ad75c7a2b911de4e8405e8ec455295
Wendel <wendelrioss@hotmail.com>
2020-10-17 09:52:57 -0300

    implement auth middleware

---
+++ src/middlewares/auth.js
@@ -0,0 +1,22 @@
+import jwt from 'jsonwebtoken';
+const authConfig = require('../config/auth.json');
+
+module.exports = (req, res, next) => {
+  const authHeader = req.header('Authorization');
+
+  if(!authHeader){
+    return res.status(401).send({error:'no token provided'});
+  }
+
+  const parts = authHeader.split(' ');
+
+  if(parts.length !== 2){
+    res.status(401).send({error:'token error'});
+  }
+
+  const [scheme,] = parts;
+
+  if(!/^Bearer$/i.test(scheme)){
+    return res.status(401).send({message:'token malformatted'})
+  }
+}
